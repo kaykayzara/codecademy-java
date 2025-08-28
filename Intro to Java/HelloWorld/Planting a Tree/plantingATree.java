bec14858e64320aa4cb4ceeb0bb80c26be8840de
kayrenemarie@gmail.com <kayrenemarie@gmail.com>
2025-08-27 22:42:17 -0400

    Track Intro to Java as regular folder

---
+++ Intro to Java/HelloWorld/Planting a Tree/plantingATree.java
@@ -0,0 +1,14 @@
+public class plantingATree {
+    public static void main(String[] args) {
+
+        System.out.println("Hey there, I'm Kayrene and I'm learning to code in Java!");
+        System.out.println("This code will print a Christmas tree.");
+
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+        System.out.println("   ***   ");
+    }
+}
---
+++ Intro to Java/README.md
@@ -0,0 +1,2 @@
+# codecademy-java
+This will hold various Codecademy projects that I have completed in the Java coding language during my learning process. Established August 2025.
